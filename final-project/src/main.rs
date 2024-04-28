410dbffb11dda5e1af08e60adea3d436e1df1b1e
Josh Dawson <jedawson2003@gmail.com>
2024-04-27 22:34:32 -0400

    Added other music files from shared google drive folder. Finally got midly to parse midi files. At the moment I'm only grabbing the track with the most events.

--- final-project/src/main.rs
+++ final-project/src/main.rs
@@ -1,21 +1,27 @@
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>; // Generic Result type
-mod parsing; // module for parsing to and from midi files
-mod markov;  // module for trainig and predicting markov model
-use std::fs;
-fn run() -> Result<()> { // calls important methods from modules 
-    let specific_file = "chopin_ballade_3_(c)lubetsky"; // specific file name 
-    let path = format!("./src/genres/chopin/{specific_file}.mid"); // KEEP THIS FOREVER
-    
-    match parsing::from_midi(&path) { 
+mod markov;
+mod parsing; // module for parsing to and from midi files // module for trainig and predicting markov model
+
+fn run() -> Result<()> {
+    // calls important methods from modules
+    let specific_file = "chopin_ballade_3_(c)lubetsky"; // specific file name
+    let path = format!("./src/genres/chopin/{specific_file}.mid"); // keep this the same
+
+    // let _parsed_midi =  assign this to a value when we get it to return one
+    match parsing::from_midi(&path) {
         Err(err) => eprint!("{err}"), // propogate errors
-        _ => println!("yay!"),        // yay!
-    }
+        _ => println!("yay"),         // yay!
+    };
+
+    markov::train_model("parsed_midi"); // call markov model
+
     Ok(())
 }
 
 fn main() {
     // calls run and handles possible errors
-    if let Err(err) = run() { // this soesn't work?
+    if let Err(err) = run() {
+        // this soesn't work?
         eprintln!("{err}");
         std::process::exit(1);
     }
--- final-project/src/markov.rs
+++ final-project/src/markov.rs
@@ -1,15 +1,27 @@
 extern crate markov; // external markov crate (also added as a dependency)
 use markov::Chain;
 
-/// train model method to create a new markov chain and train it on the data parameter
 /// documentation for markov crate: https://crates.io/crates/markov
 
-pub fn train_model(data: String) { 
+/// train model method to create a new markov chain and train it on the data parameter
+pub fn train_model(_data: &str) {
     let mut chain = Chain::new(); // create a new markov chain
-    chain.feed_str(&data); // use feed_str to feed the chain a str
 
+    // examples straight from the docs
+
+    // predicting 5 lines using chain iterators
+    chain.feed_str("I like cats and I like dogs.");
+    for line in chain.iter_for(5) {
+        println!("{:?}", line);
+    }
+    // using a higher order chain --
+    chain = Chain::of_order(2);
+    chain.feed_str("I like cats and I like dogs.");
+    for line in chain.iter_for(5) {
+        println!("{:?}", line);
+    }
 }
 
-pub fn predict_sequence() { 
+pub fn _predict_sequence() {
     todo!();
 }
