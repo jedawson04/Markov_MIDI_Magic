694401978595407592ff8079cae9a37107b463a0
Ezra Crowe <ecrowe@oberlin.edu>
2024-05-01 15:29:51 -0400

    organized some stuff

--- frontend/final-frontend/src/App.js
+++ frontend/final-frontend/src/App.js
@@ -1,53 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import InputObject from './Components/InputObject';
 
 function App() {
   return (
     <div className="App">
-      <TextBox />
-      <SubmitButton />
-      <ClassicalButton/>
-      <RockButton/>
-      <RnbButton/>
-      <JazzButton/>
+      <InputObject/>
     </div>
   );
-
-  function TextBox() {
-    return (
-      <input type="text" placeholder="Enter text here" />
-    );
-  }
-
-  function ClassicalButton() {
-    return (
-      <button>Classical</button>
-    );
-  }
-
-  function RockButton() {
-    return (
-      <button>Rock</button>
-    );
-  }
-
-  function RnbButton() {
-    return (
-      <button>Rnb</button>
-    );
-  }
-
-  function JazzButton() {
-    return (
-      <button>Jazz</button>
-    );
-  }
-  function SubmitButton() {
-    return (
-      <button>Submit</button>
-    );
-  }
-
 }
+
 
 export default App;
